4afb018288ba4b71137019b808869957b9a4efdb
USER <USER@211.227.114.41>
2023-11-28 15:32:59 +0900

    addgroup html, css 커밋

--- YOU&I/src/main/java/com/YOU_I/controller/addGroupService.java
+++ YOU&I/src/main/java/com/YOU_I/controller/addGroupService.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.YOU_I.model.Group_DAO;
+import com.YOU_I.model.Group_DTO;
+
 public class addGroupService implements Command {
 
 	@Override
@@ -13,9 +16,17 @@ public class addGroupService implements Command {
 			throws ServletException, IOException {
 		
 		request.setCharacterEncoding("utf-8");
+		String groupName = request.getParameter("groupName");
+		String groupCategory = request.getParameter("groupCategory");
 		
+		Group_DTO dto = new Group_DTO();
+		dto.setGroupCategory(groupCategory);
+		dto.setGroupName(groupName);
 		
-		return null;
+		Group_DAO dao = new Group_DAO();
+		dao.addGroup(dto);
+		
+		return "redirect:/Gomain.do";
 		
 	}
 
