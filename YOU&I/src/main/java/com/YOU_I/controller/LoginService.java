316db471c5f436af9147d6cc548f361b4a036c82
USER <USER@210.223.207.5>
2023-12-01 15:24:09 +0900

    LoginService.java 클래스에 u_id 세션에 저장

--- YOU&I/src/main/java/com/YOU_I/controller/LoginService.java
+++ YOU&I/src/main/java/com/YOU_I/controller/LoginService.java
@@ -5,41 +5,40 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.YOU_I.model.User_DAO;
 import com.YOU_I.model.User_DTO;
 
 public class LoginService implements Command {
 
-	
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		HttpSession session = request.getSession();
 		request.setCharacterEncoding("utf-8");
-		
+
 		String u_id = request.getParameter("userId");
 		String u_pw = request.getParameter("password");
-		
+
 		User_DTO u_DTO = new User_DTO();
 		u_DTO.setId(u_id);
 		u_DTO.setPw(u_pw);
-		
+
 		System.out.println(u_DTO.getId());
 		System.out.println(u_DTO.getPw());
-		
+
 		User_DAO u_DAO = new User_DAO();
-		
-		 
+
 		User_DTO result_DTO = u_DAO.user_Login(u_DTO);
-		
-		if(result_DTO != null ) {
-			
-			request.setAttribute("Login", result_DTO);
+
+		if (result_DTO != null) {
+
+			session.setAttribute("Login", u_id);
 			return "redirect:/Gomainpg.do";
 		} else {
 			return "redirect:/Gologin.do";
 		}
-		
+
 	}
 
 }
