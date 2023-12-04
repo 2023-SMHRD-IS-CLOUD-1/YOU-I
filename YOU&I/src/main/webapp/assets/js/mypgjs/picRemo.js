5e804a6144030519824e7e6fb07c81927b00cf28
USER <USER@121.179.83.108>
2023-12-04 19:35:46 +0900

    마이페이지 버튼 수정

--- YOU&I/src/main/webapp/assets/js/mypgjs/picRemo.js
+++ YOU&I/src/main/webapp/assets/js/mypgjs/picRemo.js
@@ -1,10 +1,9 @@
-function picRemo() {
+
     const remo = document.getElementById('remo');
     const picText = document.getElementById('pic');
 
     remo.addEventListener('click', function () {
-        uploadedImage.src = '';
+        $("uploadedImage").attr("src","");
         uploadedImage.style.display = 'none';
         picText.style.display = 'block';
     });
-}
