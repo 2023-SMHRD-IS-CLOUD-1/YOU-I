eaf1488729b020ed1f67336b52edfff48026093e
USER <USER@211.227.114.41>
2023-12-05 19:17:34 +0900

    메인페이지 그룹클릭시 joinGroup.html로 groupNo 가지고 이동

--- YOU&I/src/main/webapp/assets/js/mainpgjs/mainrank.js
+++ YOU&I/src/main/webapp/assets/js/mainpgjs/mainrank.js
@@ -6,12 +6,31 @@ function rank() {
 	return new Promise(function(resolve, reject) {
 		$.ajax({
 			//data : sendObj,
-			url: "http://localhost:8081/YOU_I/rank.do",
+			url: "rank.do",
 			dataType: "json",
 			success: function(res) {
+				console.log(res)
+				$("#tagA1").on("click", function() {
+						console.log("클릭")
+						$("#tagA1").attr("href","GojoinGroup.do?groupNo=" + res[0].groupNo);
+					})
+				$("#tagA2").on("click", function() {
+						console.log("클릭")
+						$("#tagA2").attr("href","GojoinGroup.do?groupNo=" + res[1].groupNo);
+					})
+				$("#tagA3").on("click", function() {
+						console.log("클릭")
+						$("#tagA3").attr("href","GojoinGroup.do?groupNo=" + res[2].groupNo);
+					})
+				$("#tagA4").on("click", function() {
+						console.log("클릭")
+						$("#tagA4").attr("href","GojoinGroup.do?groupNo=" + res[3].groupNo);
+					})
+				
 				var i = 0;
 				StringNo = "";
 				res.forEach(function(res) {
+					console.log("확인용" + res.gr)
 					var temp = res.groupNo
 					StringNo += temp + ",";
 					Groupno.push(StringNo);
@@ -19,10 +38,11 @@ function rank() {
 					$(".groupinfo" + i).after("<tr><td class='grouptd'>" + res.groupInfo + "</td></tr>");
 					$(".groupinfo" + i).after("<tr><td class='grouptd'>" + res.groupName + "</td></tr>");
 					$(".groupinfo" + i).after("<tr><td class='grouptd'>" + flaticonHobby(res.hobbyNo) + res.hobbyName + "</td></tr>");
+					
 					i++;
 
 				});
-					resolve(StringNo);
+				resolve(StringNo);
 
 
 			},
@@ -31,7 +51,7 @@ function rank() {
 				console.log(e);
 			}
 		})
-		
+
 
 	})
 
@@ -43,25 +63,25 @@ function rank() {
 rank().then(function(resolvedData) {
 	console.log(resolvedData);
 	var Groupnodata = { data: resolvedData }
- 	var bucketUrl = "https://s3.ap-northeast-2.amazonaws.com/you-i/"
+	var bucketUrl = "https://s3.ap-northeast-2.amazonaws.com/you-i/"
 	$.ajax({
 
-		url: "http://localhost:8081/YOU_I/GroupImageTake.do",
+		url: "GroupImageTake.do",
 		data: Groupnodata,
 		dataType: "json",
 		success: function(res) {
 			var i = 1;
-			res.forEach(function(res){
-			console.log("https://s3.ap-northeast-2.amazonaws.com/you-i/resize_profile"+res.fileThumb)
-			var imagePath = "resize_profile"+res.fileThumb;
-			var finalUrl = bucketUrl + encodeURIComponent(imagePath);
-				$("#groupimg"+i).append(`<img style="object-fit: fill; width:155px;height:155px" src=${finalUrl}>`);
+			res.forEach(function(res) {
+				console.log("https://s3.ap-northeast-2.amazonaws.com/you-i/resize_profile" + res.fileThumb)
+				var imagePath = "resize_profile" + res.fileThumb;
+				var finalUrl = bucketUrl + encodeURIComponent(imagePath);
+				$("#groupimg" + i).append(`<img style="object-fit: fill; width:155px;height:155px" src=${finalUrl}>`);
 				i++;
 			})
 		},
 		error: function(e) {
-				console.log(e);
-			}
+			console.log(e);
+		}
 	})
 })
 
