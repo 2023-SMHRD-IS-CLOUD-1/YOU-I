8c46fc352875a10de841db05128a2eb457468b96
USER <USER@211.227.114.41>
2023-12-11 14:31:43 +0900

    addgroup.html , groupKing.html, groupSchedules.html, joinGroup.html, map.html(삭제), calendar.html(삭제),modifyprofile.html(삭제) map.js, findking.js

--- YOU&I/src/main/webapp/assets/js/communiyjs/findking.js
+++ YOU&I/src/main/webapp/assets/js/communiyjs/findking.js
@@ -16,14 +16,9 @@ $.ajax({
     url: 'findKing.do?groupNo='+GroupNo,
     dataType: 'json',
     success: function (findKingData) {
-        console.log(findKingData);
-        console.log(findKingData.levelNo)
         if(findKingData.levelNo==21){
 			$("#sideService").append(`<li><a id='groupKing' href='GogroupKing.do?groupNo=${GroupNo}'>가입승인</a></li>`)
 		}
     },
-     error: function (xhr, status, error) {
-        console.error('Error occurred in findKing.do:', error);
-        console.log(xhr)
-    }
+    
 });
